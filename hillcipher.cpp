16d4169ca83151b50a73befe97bc8accb2e20aa1
Bishmit Regmi <103090191+Bishmit@users.noreply.github.com>
2024-04-17 15:55:16 +0545

    done with encryption part decryption remains

--- hillcipher.cpp
+++ hillcipher.cpp
@@ -1,77 +1,139 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
 using namespace std;
 
-void computeDeterminant(int mat[3][3], double & determinant){
-    determinant = (mat[0][0] * ((mat[1][1] * mat[2][2]) - (mat[1][2] * mat[2][1]))) -
-                  (mat[0][1] * ((mat[1][0] * mat[2][2]) - (mat[1][2] * mat[2][0]))) +
-                  (mat[0][2] * ((mat[1][0] * mat[2][1]) - (mat[1][1] * mat[2][0])));
-    cout << "Determinant: " << determinant << endl;
-}
+#define row 3
+#define col 3
 
-void transposeMatrix(int mat[3][3], int transpose[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            transpose[j][i] = mat[i][j];
-        }
-    }
-}
+// Define a structure to store three integers
+struct Triplet {
+    int x;
+    int y;
+    int z;
+};
 
-void performOperation(int transpose[3][3], int newmat[3][3], int adj[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            newmat[i][j] = (transpose[(i + 1) % 3][(j + 1) % 3] * transpose[(i + 2) % 3][(j + 2) % 3])
-                           - (transpose[(i + 1) % 3][(j + 2) % 3] * transpose[(i + 2) % 3][(j + 1) % 3]);
+// Function declarations
+void importMatrix(int mat[row][col]);
+void importPlaintext(string& plaintext,int mat[row][col]);
+Triplet doEncryption(char first, char second, char third, int mat[col][row]);
 
-            if ((i + j) % 2 != 0) {
-                newmat[i][j] = (-1) * newmat[i][j];
-            }
-           // cout << newmat[i][j] << " ";
-        }
-       // cout << endl;
-    }
-   // cout << endl;
-
-    // Finding adjoint now 
-    cout<<"The adjoint is: "<<endl; 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            adj[i][j] = newmat[i][j];
-            if ((i + j) % 2 != 0) adj[i][j] = (-1) * adj[i][j];
-            cout << adj[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void findinverse(int adj[3][3], double determinant, double inversemat[3][3]) {
-    cout << endl << "Inverse matrix:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            inversemat[i][j] = adj[i][j] / determinant;
-            cout << inversemat[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-int main() {
-    int mat[3][3];
-    int transpose[3][3], newmat[3][3], adj[3][3];
-    double determinant;
-    double inversemat[3][3];
-    
+void importMatrix(int mat[row][col]) {
     cout << "Enter the elements of the matrix:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             cin >> mat[i][j];
         }
     }
+}
+
+void importPlaintext(string& plaintext, int mat[row][col]) {
+    cout << "Enter the plaintext: ";
+    cin >> plaintext;
+
+    string newtext;
+    for (int i = 0; i < plaintext.size(); i++) {
+        newtext += plaintext[i];
+    }
+
+    if (newtext.size() % 3 != 0) {
+        newtext += 'z'; // Adding 'z' at the end if the size is not divisible by 3
+    }
+
+    plaintext = newtext;
+
+    // Now partitioning the plaintext into three equal parts
+    for (int i = 1; i <= plaintext.size(); i++) {
+        for (int counter = 1; counter <= plaintext.size() / 3; counter++) {
+            if (i == counter * 3) {
+                char first = plaintext[i - 3];
+                char second = plaintext[i - 2];
+                char third = plaintext[i - 1];
+
+                Triplet result = doEncryption(first, second, third,mat);
+                char ch1, ch2, ch3; 
+                if (result.x >= 0 && result.x <= 25) {
+                    ch1 = 'A' + result.x;
+                   
+                }
+                
+                if (result.y >= 0 && result.y <= 25) {
+                    ch2 = 'A' + result.y;
+                   
+                }
+                
+                if (result.z >= 0 && result.z <= 25) {
+                    ch3 = 'A' + result.z;
+                    
+                }
+                
+                cout<<first<<second<<third<<" = "<<ch1<<" "<<ch2<<" "<<ch3<<endl; 
+            }
+        }
+    }
+}
+
+Triplet doEncryption(char first, char second, char third, int mat[col][row]) {
+    Triplet result;
+
+    if (first >= 'a' && first <= 'z') {
+        result.x = int(first) - 97;
+    } else if (first >= 'A' && first <= 'Z') {
+        result.x = int(first) - 65;
+    } else {
+        cout << "Not a letter!";
+    }
+
+    if (second >= 'a' && second <= 'z') {
+        result.y = int(second) - 97;
+    } else if (second >= 'A' && second <= 'Z') {
+        result.y = int(second) - 65;
+    } else {
+        cout << "Not a letter!";
+    }
+
+    if (third >= 'a' && third <= 'z') {
+        result.z = int(third) - 97;
+    } else if (third >= 'A' && third <= 'Z') {
+        result.z = int(third) - 65;
+    } else {
+        cout << "Not a letter!";
+    }
+  
+    int newX = result.x * mat[0][0] + result.y * mat[1][0] + result.z * mat[2][0];
+    int newY = result.x * mat[0][1] + result.y * mat[1][1] + result.z * mat[2][1];
+    int newZ = result.x * mat[0][2] + result.y * mat[1][2] + result.z * mat[2][2];
     
-    computeDeterminant(mat, determinant); 
-    transposeMatrix(mat, transpose);
-    performOperation(transpose, newmat, adj);
-    findinverse(adj, determinant, inversemat);
+    cout<<endl; 
+
+    result.x = (newX) % 26;
+    result.y = (newY) % 26;
+    result.z = (newZ) % 26;
+    
+    return result;
+}
+
+int main() {
+    int mode, mat[row][col];
+    string plaintext;
+    cout << "Choose mode:" << endl;
+    cout << "1. Encryption" << endl;
+    cout << "2. Decryption" << endl;
+
+    cin >> mode; 
+
+    switch (mode) {
+    case 1:
+        importMatrix(mat);
+        importPlaintext(plaintext,mat);
+        break;
+
+    case 2:
+       // decryption part remains
+        break;
+
+    default:
+        cout << "Invalid mode!" << endl;
+        break;
+    }
 
     return 0;
 }
