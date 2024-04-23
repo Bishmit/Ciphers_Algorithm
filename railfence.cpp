d4ea9a0f4922ccffe92e265278acdb1e0944fa8c
Bishmit Regmi <103090191+Bishmit@users.noreply.github.com>
2024-04-23 19:16:53 +0545

    done and dusted

--- railfence.cpp
+++ railfence.cpp
@@ -44,24 +44,93 @@ void encryptRailFence(const string& plaintext, int depth) {
     }
 }
 
+void decryptRailFence(const string& ciphertext, int depth) {
+    int n = ciphertext.size();
+    string s[depth][n];
+
+    int row = 0;
+    int col = 0;
+    bool down = true;
+
+    // Create the zigzag pattern in the matrix
+    for(int i = 0; i < n; i++) {
+        s[row][col] = "*"; 
+
+        if(row == 0) {
+            down = true;
+        } else if(row == depth-1) {
+            down = false;
+        }
+
+        if(down) {
+            row++;
+        } else {
+            row--;
+        }
+
+        col++;
+    }
+
+    int index = 0;
+    // Fill in the characters in the zigzag pattern
+    for(int i = 0; i < depth; i++) {
+        for(int j = 0; j < n; j++) {
+            if(s[i][j] == "*") {
+                s[i][j] = ciphertext[index++];
+            }
+        }
+    }
+
+    cout << "\nDecrypted output: ";
+    // printing coloumn wise to get output
+    row = 0;
+    col = 0;
+    down = true;
+
+    for(int i = 0; i < n; i++) {
+        cout << s[row][col];
+
+        if(row == 0) {
+            down = true;
+        } else if(row == depth-1) {
+            down = false;
+        }
+
+        if(down) {
+            row++;
+        } else {
+            row--;
+        }
+
+        col++;
+    }
+    cout << endl;
+}
+
 int main() {
-    string plaintext; 
+    string text; 
     int depth, mode; 
     cout << "1. Encryption" << endl;
     cout << "2. Decryption" << endl;
     cout << "Choose mode: ";
     cin>>mode; 
+    cin>>ignore(); 
+    
         switch(mode) {
             case 1: 
                 cout << "Enter the plaintext for encryption: "; 
-                cin >> plaintext; 
+                getline(cin, text);  
                 cout << "Enter the depth: "; 
                 cin >> depth; 
                 encryptRailFence(plaintext, depth);
                 break;
 
             case 2: 
-                cout << "Decryption feature will be implemented soon.\n";
+                cout << "Enter the ciphertext for decryption: "; 
+                getline(cin, text); 
+                cout << "Enter the depth: "; 
+                cin >> depth; 
+                decryptRailFence(text, depth);
                 break;
 
             default:
